57340e01716d254ffbb987852ebee9fa4a217141
Rafael Morales <rmorales@utem.cl>
2022-06-22 20:18:57 -0400

    Home Screen

---
+++ lib/components/touchable_list_item.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TouchableListItem extends StatelessWidget {
+  const TouchableListItem({
+    Key? key,
+    this.onTap,
+    required this.icon,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
+
+  final void Function()? onTap;
+  final IconData icon;
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Row(
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                color: const Color(0xffeeeeee),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Icon(
+                icon,
+                size: 56,
+                color: Colors.black,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      alignment: Alignment.bottomLeft,
+                      height: 40.0,
+                      child: Text(
+                        title,
+                        style: GoogleFonts.inter(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      height: 40.0,
+                      child: Text(
+                        description,
+                        style: GoogleFonts.inter(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xff3e3e3e),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cpyd03/components/touchable_list_item.dart';
 import 'package:cpyd03/screens/meta_screens/blank_card_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -33,14 +34,61 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return BlankCardScreen(
       topWidget: Text(
-        greetings ?? "Bienvenid@",
+        greetings ?? "wena po",
         style: GoogleFonts.inter(
           fontSize: 36,
           fontWeight: FontWeight.w700,
           color: Colors.white,
         ),
       ),
-      child: const Text("alo"),
+      child: Column(
+        children: [
+          Expanded(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                "Clase actual",
+                style: GoogleFonts.inter(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: Text(
+                  "No estás presente en ninguna clase ahora mismo...",
+                  style: GoogleFonts.inter(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xff3e3e3e),
+                  ),
+                ),
+              )
+            ]),
+          ),
+          const Divider(),
+          TouchableListItem(
+            title: "Ingresar",
+            description: "Lee el código QR de ingreso para tu clase",
+            icon: Icons.qr_code_scanner,
+            onTap: (() => print("peo 1")),
+          ),
+          const Divider(),
+          TouchableListItem(
+            title: "Asistencia",
+            description: "Revisa tu asistencia a clases",
+            icon: Icons.supervised_user_circle,
+            onTap: (() => print("peo 2")),
+          ),
+          const Divider(),
+          TouchableListItem(
+            title: "Sobre la App",
+            description: "Revisa detalles sobre esta App",
+            icon: Icons.laptop_chromebook,
+            onTap: (() => print("peo 3")),
+          ),
+        ],
+      ),
     );
   }
 }
